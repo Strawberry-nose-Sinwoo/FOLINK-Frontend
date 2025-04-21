66b5e3c22cb7ef95a70b4e7e76d80ec179df7b4d
Hongje Seong <shjea4364@naver.com>
2025-04-22 08:38:43 +0900

    Revert "🚀 [Feature] #3 - 메인 페이지 디자인 퍼블리싱"

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from "react-router-dom"
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    <Router>
-      <App />
-      <ToastContainer />
-    </Router>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
