51c5fc09e3a86c0d9c25e32590767b800435d7a2
806gw <shjea4364@naver.com>
2025-04-21 16:21:41 +0900

    feat: Main 페이지 디자인 퍼블리싱 - 토스트 type 추가

--- src/components/Toastify/index.tsx
+++ src/components/Toastify/index.tsx
@@ -1,14 +1,14 @@
-import { Slide, toast, ToastOptions, ToastPosition } from "react-toastify";
+import { Slide, toast, ToastOptions, ToastPosition } from 'react-toastify';
 
 interface ToastProps {
-  type: "loading"
+  type: 'success' | 'error' | 'loading';
   message: string;
-  position?: ToastPosition; 
+  position?: ToastPosition;
 }
 
 const defaultToastOptions: ToastOptions = {
-  position: "top-center",
-  autoClose: false,
+  position: 'top-center',
+  autoClose: 2000,
   pauseOnHover: false,
   pauseOnFocusLoss: false,
   closeOnClick: false,
@@ -17,18 +17,30 @@ const defaultToastOptions: ToastOptions = {
 };
 
 export function Toastify({ message, type, position }: ToastProps) {
-    const toastConfig = {
-      ...defaultToastOptions,
-      position: position || defaultToastOptions.position,
-    };
-  
-    switch (type) {
-      case "loading":
-        toast.loading(message, {
-          ...toastConfig,
-        })
-        return;
-      default:
-        toast(message, toastConfig);
-    }
+  const toastConfig = {
+    ...defaultToastOptions,
+    position: position || defaultToastOptions.position,
+  };
+
+  switch (type) {
+    case 'success':
+      toast.success(message, {
+        ...toastConfig,
+      });
+      return;
+
+    case 'error':
+      toast.error(message, {
+        ...toastConfig,
+      });
+      return;
+
+    case 'loading':
+      toast.loading(message, {
+        ...toastConfig,
+      });
+      return;
+    default:
+      toast(message, toastConfig);
   }
+}
